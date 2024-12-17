4c8208b4a8007ec52e162861376a8c7b2dd4a4a7
Gurbala Laurent <69318652+laurentGurbala@users.noreply.github.com>
2024-12-17 14:46:40 +0100

    style(comments): Amélioration de l'affichage des commentaires admin
    Ajout d'un style tableau avec alternance de couleurs

--- views/templates/adminComments.php
+++ views/templates/adminComments.php
@@ -11,15 +11,14 @@
 </div>
 
 <h2>Commentaire de l'article:</h2>
-<p><?= $article->getTitle() ?></p>
 
 <div class="commentList">
+    <p class="articleTitle"><?= $article->getTitle() ?></p>
     <ul>
-
         <?php foreach ($comments as $comment): ?>
             <li>
-                <p><?= $comment->getContent() ?></p>
-                <div>
+                <p class="commentContent"><?= $comment->getContent() ?></p>
+                <div class="commentAction">
                     <a class="submit" href="index.php?action=deleteComment&commentId=<?= $comment->getId() ?>&articleId=<?= $article->getId() ?>"
                         <?= Utils::askConfirmation("Êtes-vous sûr de vouloir supprimer ce commentaire ?") ?>>
                         Supprimer
